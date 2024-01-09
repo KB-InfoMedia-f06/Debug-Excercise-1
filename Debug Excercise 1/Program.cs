992434b7af0d882d50c8513cf9ca4123d3bb7d81
Hanna-Johansson-KB <Hanna.Johansson@amal.se>
2024-01-09 12:41:09 +0100

    completed logic and created all syntax errors

--- Debug Excercise 1/Program.cs
+++ Debug Excercise 1/Program.cs
@@ -1,5 +1,29 @@
-﻿class Program {
-    public static void Main(string[] args) {
+﻿/* abascus
+ * 
+ * takes numbers until user dont want to enter more
+ * sums all numbers togeheter
+ * 
+ */
+class Program {
+    public Main(string[] args) {
+        bool pressedContinue = true;
+        int sum = 0;
 
+
+        Console.WriteLine("enter a number");
+        int input = Convert.ToInt32(console.ReadLine())
+        sum += input;
+
+        while (presedContinue) {
+            Console.WriteLine("add another number");
+            input = Convert.ToInt32(Console,ReadLine());
+            summa += input;
+            Console.WriteLine("do yo want to add more numbers? [Y/N]");
+            response = Console.ReadLine();
+            if(response == "N") {
+                pressedContinue == false;
+            }
+        }
+
+        Console.WriteLine($"The total sum is: {sum}");
     }
-}
